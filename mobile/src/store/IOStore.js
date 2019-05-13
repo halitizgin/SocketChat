9420855bf985223650df31f56ee24ce5158e797e
Halit IZGIN <halit_izgin@outlook.com.tr>
2019-05-13 23:09:27 +0300

    Update IOStore.js

--- mobile/src/store/IOStore.js
+++ mobile/src/store/IOStore.js
@@ -2,7 +2,7 @@ import {observable, action} from 'mobx';
 
 class IOStore{
     @observable io = null;
-    @observable ip = '192.168.179.225'; //Ip adresi yanlış olursa çalışmaz.
+    @observable ip = '192.168.x.x'; //Ip adresi yanlış olursa çalışmaz.
 
     @action setIO = (socket) => {
         this.io = socket;
